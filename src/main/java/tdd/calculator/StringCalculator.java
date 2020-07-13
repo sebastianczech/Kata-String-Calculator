09e3818e34c6e6490c85bc641b8c162b311dc2de
michal.lipski <kiwaczki@gmail.com>
2020-07-13 23:58:28 +0200

    initial

---
+++ src/main/java/tdd/calculator/StringCalculator.java
@@ -0,0 +1,5 @@
+package tdd.calculator;
+
+public class StringCalculator {
+
+}
---
+++ src/test/java/tdd/calculator/StringCalculatorTest.java
@@ -0,0 +1,13 @@
+package tdd.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void startHere() {
+        assertThat(4).isEqualTo(5);
+    }
+}
