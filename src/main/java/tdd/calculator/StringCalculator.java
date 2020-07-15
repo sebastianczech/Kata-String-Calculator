80cbc53bbe50de4a9ca2a5230f6f31c0d58d921d
bukowmic <bukowmic@gmail.com>
2020-07-15 10:37:15 +0200

    suma dla dwóch liczb

--- src/main/java/tdd/calculator/StringCalculator.java
+++ src/main/java/tdd/calculator/StringCalculator.java
@@ -1,12 +1,16 @@
 package tdd.calculator;
 
+import java.util.Arrays;
+
 public class StringCalculator {
 
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
-        } else {
-            return Integer.parseInt(numbers);
         }
+
+        return Arrays.stream(numbers.split(","))
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
