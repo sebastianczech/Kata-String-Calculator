91e0f2b601f2dabd24c4d05a9547be5f77c67c8a
bukowmic <bukowmic@gmail.com>
2020-07-15 10:06:42 +0200

    drugi test

--- src/test/java/tdd/calculator/StringCalculatorTest.java
+++ src/test/java/tdd/calculator/StringCalculatorTest.java
@@ -16,5 +16,13 @@ public class StringCalculatorTest {
         assertThat(result).isEqualTo(0);
     }
 
-    
+    @Test
+    void should_return_number_for_number() {
+        StringCalculator stringCalculator = new StringCalculator();
+        String numbers = "10";
+
+        int result = stringCalculator.add(numbers);
+
+        assertThat(result).isEqualTo(10);
+    }
 }
